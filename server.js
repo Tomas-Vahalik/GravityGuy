1ca0d7de891eaa826cd4130d1203071101cc7593
Jakub Rathouský <rathojak@cvut.cz>
2021-01-09 11:56:22 +0100

    not postbuild but prebuild

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ let port = process.env.PORT || 5000;
   })
 
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/view/index.html'));
+    res.sendFile(path.join(__dirname + '/build_project/index.html'));
   })
 
 
